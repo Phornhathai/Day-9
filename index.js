368b6d4045dab544e1c5a5dcd74a9ea184350cf4
Phornhathai Thanomwong <aummiidesu@MacBook-Air-khxng-Aum.local>
2023-09-21 10:09:12 +0700

    add public folder

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+// Requiring module
 const express = require('express')
+const path = require('path');
+// Creating express object
 const app = express()
-const port = 3000
+const port = 8000;
+
 
 app.set('view engine','ejs');
+
+// Function to serve all static files
 app.use(express.static('public'));
+// app.use('/static',express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   // res.send('<h1>Phornhathai Thanomwong</h1>')
   res.render(__dirname + '/views/index.ejs', {name : "Phornhathai" , lastname : "Thanomwong"});
+});
+app.get('/about', (req, res) => {
+  // res.send('<h1>Phornhathai Thanomwong</h1>')
+  res.render(__dirname + '/views/about.ejs');
 });
 
 app.listen(port, () => {
